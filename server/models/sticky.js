7d887c1318a7686aecb0c9ca45f8a71b0e9f4654
MichelleN <dr.mylann@gmail.com>
2016-10-11 01:16:47 +0000

    added schemas

--- server/models/sticky.js
+++ server/models/sticky.js
@@ -1,1 +1,11 @@
-//he
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var StickySchema = new Schema({
+    title: String,
+    date: Date,
+    content: String,
+    rating: { type: Number, min: 1, max: 3 }
+});
+
+module.exports = mongoose.model('Sticky', StickySchema);
