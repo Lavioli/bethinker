7050cc53b58775d0a810976f4d79651fce4ee36a
MichelleN <dr.mylann@gmail.com>
2016-10-11 16:00:01 +0000

    added authorization

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
     username: {
@@ -12,5 +13,15 @@ var UserSchema = new mongoose.Schema({
         required: true
     }
 });
+
+User.Schema.methods.validatePassword = function(password, callback) {
+	bcrypt.compare(password, this.password, function(err, isValid) {
+		if (err) {
+			callback(err);
+			return;
+		}
+		callback(null, isValid);
+	});
+}
 
 module.exports = mongoose.model('User', UserSchema);
