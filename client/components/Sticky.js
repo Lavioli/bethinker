7ecd8857a5d43a62f6fbc588766d783a875490be
LavieMaru <laviemaru@gmail.com>
2016-10-14 08:14:23 -0400

    fully functional delete button yay

--- client/components/Sticky.js
+++ client/components/Sticky.js
@@ -22,7 +22,7 @@ var Sticky = React.createClass({
 			<div className = "container">
 				<div className="sticky_container" id={stickyId}>
 					<div>
-						<input type="submit" value="Delete" onClick={this.onClickDelete()} />
+						<input type="submit" value="Delete" onClick={this.onClickDelete} />
 					</div>
 					<div className="titles">{stickyTitle}</div>
 					<div className="content">{stickyContent}</div>
@@ -32,21 +32,5 @@ var Sticky = React.createClass({
 	}
 });
 
-//working append
-/*var Sticky = function (props) {
-	var deleteThis = props.deleteSticky;
-
-	return (
-		<div className = "container">
-			<div className="sticky_container" id={props.stickyId}>
-				<div>
-					<button type="button" onClick={deleteThis(props.stickyId)}>Delete</button>
-				</div>
-				<div className="titles">{props.title}</div>
-				<div className="content">{props.content}</div>
-			</div>
-		</div>	
-	);
-};*/
 
 export default Sticky;
