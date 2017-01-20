429cd9d811ebba6b800321802e9dcfecb6d1de12
KevL927 <leekevin@gmail.com>
2017-01-20 15:57:06 -0500

    Externalize Sticky endpoints to a separate file

---
+++ server/endpoints/stickyRouter.js
@@ -0,0 +1,133 @@
+import express from 'express';
+import passport from 'passport';
+
+import Sticky from '../models/sticky';
+
+const stickyRouter = express.Router();
+//************************************STICKIE ENDPOINTS*****************************************/
+
+//get all the stickies for now, this is for testing purposes atm
+// app.get('/stickies', passport.authenticate('basic', {session: false}),
+// function(req, res) {
+//     Sticky.find(function(err, sticky) {
+//         if (err) {
+//             return res.status(500).json({message: 'Internal Server Error'});
+//         }
+
+//         return res.status(200).json(sticky);
+//     });
+// });
+//Allows authorizied users to see their sticky notes
+stickyRouter.get('/users/:username/stickies', passport.authenticate('basic', {session: false}), function(req, res) {
+
+    var routerUsername = req.params.username;
+    var authenticatedUsername = req.user.username.toString();
+    var authenticatedId = req.user._id.toString();
+
+    if(routerUsername !== authenticatedUsername) {
+         return res.status(422).json({
+            "message": "Unauthorized"
+         });
+    }
+
+    Sticky.find({_user: authenticatedId}, function(err, stickies) {
+        if(err) {
+            return res.status(err);
+        }
+        return res.status(200).json(stickies);
+    });
+
+});
+//should allow authorized users to delete their sticky
+stickyRouter.delete("/users/:username/stickies/:stickyId", passport.authenticate('basic', {session: false}), function(req, res) {
+    //putting all requests in variables
+            var username = req.params.username;
+            var stickyId = req.params.stickyId;
+            var authenticatedUsername = req.user.username.toString();
+
+    //finds to allow user to delete sticky by stick:id
+    Sticky.findByIdAndRemove({_id: stickyId}, function(err, sticky) {
+            if(err) {
+                return res.sendStatus(500);
+            }
+            if(username !== authenticatedUsername) {
+                return res.status(401).json({message: "Unauthorized"});
+            }
+        return res.status(200).json({});
+
+    });
+});
+//Allows users to create the title for their sticky notes
+stickyRouter.post('/users/:username/stickies', passport.authenticate('basic', {session: false}), function(req, res) {
+            var routeUsername = req.params.username;
+            var stickyId = req.params.stickyId;
+            var authenticatedUsername = req.user.username.toString();
+            var authenticatedId = req.user._id.toString();
+            var title = req.body.title;
+            var content = req.body.content;
+
+    Sticky.create({title: title, content: content, _user: authenticatedId}, function(err, sticky) {
+        if(err) {
+            return res.sendStatus(500);
+        }
+        if(routeUsername !== authenticatedUsername) {
+            return res.status(401).json({message: "Unauthorized"});
+        }
+
+        return res.status(201).location("/users/" + authenticatedUsername + "/stickies/" + sticky._id).json({stickyId: sticky._id});
+    });
+
+});
+
+//Allows user to edit a sticky note
+stickyRouter.put("/users/:username/stickies/:stickyId", passport.authenticate('basic', {session: false}), function(req, res) {
+    //putting all requests in variables
+            var routerUsername = req.params.username;
+            var stickyId = req.params.stickyId;
+            var title = req.body.title;
+            var content = req.body.content;
+            var authenticatedUsername = req.user.username.toString();
+            var authenticatedId = req.user._id.toString();
+
+    //finds stick-id and then allow user to edit sticky
+    Sticky.findOne({_user: authenticatedId, _id: stickyId}, function(err, sticky) {
+
+            if(routerUsername !== authenticatedUsername) {
+
+                return res.status(401).json({message: "Unauthorized"});
+            }
+
+            //check to see if title or content was changed
+            if (title && content){
+
+                Sticky.findOneAndUpdate(
+                    {_user: authenticatedId, _id: stickyId},
+                    {content: content, title: title},
+                    function(err, sticky) {
+
+                        if(err) {
+
+                            return res.sendStatus(500);
+                        }
+
+                        return res.status(200).json({message: "Succesfully saved"});
+                        }
+                );
+            }
+            // if (content && !title){
+            //     Sticky.findOneAndUpdate(
+            //         {_user: authenticatedId, _id: stickyId},
+            //         {title: sticky.title, content: content},
+            //         function(err, sticky) {
+            //             if(err) {
+            //                 return res.sendStatus(500);
+            //             }
+            //         return res.status(201).json({message: "Succesfully saved"});
+            //     });
+            // }
+
+    });
+
+});
+
+export default stickyRouter;
--- server/index.js
+++ server/index.js
@@ -1,28 +1,33 @@
-import 'babel-polyfill'; 
+import 'babel-polyfill';
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import bcrypt from 'bcryptjs';
 import passport from 'passport';
-import strategy from './endpoints/strategy';
 
+import strategy from './endpoints/strategy';
+import stickyRouter from './endpoints/stickyRouter';
 import User from './models/user';
-import Sticky from './models/sticky';
+// import Sticky from './models/sticky';
+
+const app = express();
+// const jsonParser = bodyParser.json();
+const HOST = process.env.HOST;
+const PORT = process.env.PORT || 8080;
 
 mongoose.connect(process.env.DATABASE_URI || global.databaseUri || 'mongodb://localhost:27017/stickies');
 
 passport.use(strategy);
+app.use(express.static(process.env.CLIENT_PATH));
+app.use(bodyParser.json());
+app.use('/', stickyRouter)
 
 //app.use(passport.initialize());
-const jsonParser = bodyParser.json();
-const HOST = process.env.HOST;
-const PORT = process.env.PORT || 8080;
+
 
 console.log(`Server running in ${process.env.NODE_ENV} mode`);
 
-const app = express();
 
-app.use(express.static(process.env.CLIENT_PATH));
 
 function runServer() {
     return new Promise((resolve, reject) => {
@@ -42,131 +47,7 @@ if (require.main === module) {
     runServer();
 }
 
-//************************************STICKIE ENDPOINTS*****************************************/
 
-//get all the stickies for now, this is for testing purposes atm
-// app.get('/stickies', passport.authenticate('basic', {session: false}),
-// function(req, res) {
-//     Sticky.find(function(err, sticky) {
-//         if (err) {
-//             return res.status(500).json({message: 'Internal Server Error'});
-//         }
-
-//         return res.status(200).json(sticky);
-//     });
-// });
-//Allows authorizied users to see their sticky notes
-app.get('/users/:username/stickies', jsonParser, passport.authenticate('basic', {session: false}), function(req, res) {
-
-    var routerUsername = req.params.username;
-    var authenticatedUsername = req.user.username.toString();
-    var authenticatedId = req.user._id.toString();
-
-    if(routerUsername !== authenticatedUsername) {
-         return res.status(422).json({
-            "message": "Unauthorized"
-         });
-    }
-
-    Sticky.find({_user: authenticatedId}, function(err, stickies) {
-        if(err) {
-            return res.status(err);
-        }
-        return res.status(200).json(stickies);
-    });
-
-});
-//should allow authorized users to delete their sticky
-app.delete("/users/:username/stickies/:stickyId", jsonParser, passport.authenticate('basic', {session: false}), function(req, res) {
-    //putting all requests in variables
-            var username = req.params.username;
-            var stickyId = req.params.stickyId;
-            var authenticatedUsername = req.user.username.toString();
-
-    //finds to allow user to delete sticky by stick:id
-    Sticky.findByIdAndRemove({_id: stickyId}, function(err, sticky) {
-            if(err) {
-                return res.sendStatus(500);
-            }
-            if(username !== authenticatedUsername) {
-                return res.status(401).json({message: "Unauthorized"});
-            }
-        return res.status(200).json({});
-
-    });
-});
-//Allows users to create the title for their sticky notes
-app.post('/users/:username/stickies', jsonParser, passport.authenticate('basic', {session: false}), function(req, res) {
-            var routeUsername = req.params.username;
-            var stickyId = req.params.stickyId;
-            var authenticatedUsername = req.user.username.toString();
-            var authenticatedId = req.user._id.toString();
-            var title = req.body.title;
-            var content = req.body.content;
-
-    Sticky.create({title: title, content: content, _user: authenticatedId}, function(err, sticky) {
-        if(err) {
-            return res.sendStatus(500);
-        }
-        if(routeUsername !== authenticatedUsername) {
-            return res.status(401).json({message: "Unauthorized"});
-        }
-
-        return res.status(201).location("/users/" + authenticatedUsername + "/stickies/" + sticky._id).json({stickyId: sticky._id});
-    });
-
-});
-
-//Allows user to edit a sticky note
-app.put("/users/:username/stickies/:stickyId", jsonParser, passport.authenticate('basic', {session: false}), function(req, res) {
-    //putting all requests in variables
-            var routerUsername = req.params.username;
-            var stickyId = req.params.stickyId;
-            var title = req.body.title;
-            var content = req.body.content;
-            var authenticatedUsername = req.user.username.toString();
-            var authenticatedId = req.user._id.toString();
-
-    //finds stick-id and then allow user to edit sticky
-    Sticky.findOne({_user: authenticatedId, _id: stickyId}, function(err, sticky) {
-
-            if(routerUsername !== authenticatedUsername) {
-
-                return res.status(401).json({message: "Unauthorized"});
-            }
-
-            //check to see if title or content was changed
-            if (title && content){
-
-                Sticky.findOneAndUpdate(
-                    {_user: authenticatedId, _id: stickyId},
-                    {content: content, title: title},
-                    function(err, sticky) {
-
-                        if(err) {
-
-                            return res.sendStatus(500);
-                        }
-
-                        return res.status(200).json({message: "Succesfully saved"});
-                        }
-                );
-            }
-            // if (content && !title){
-            //     Sticky.findOneAndUpdate(
-            //         {_user: authenticatedId, _id: stickyId},
-            //         {title: sticky.title, content: content},
-            //         function(err, sticky) {
-            //             if(err) {
-            //                 return res.sendStatus(500);
-            //             }
-            //         return res.status(201).json({message: "Succesfully saved"});
-            //     });
-            // }
-
-    });
-
-});
 
 /************************************USER ENDPOINTS***************************************/
 
@@ -184,7 +65,7 @@ app.get('/user', function(req,res){
 
 
 //should list only the current user's user info
-app.get('/users/:username', jsonParser, passport.authenticate('basic', {session: false}), function(req, res) {
+app.get('/users/:username', passport.authenticate('basic', {session: false}), function(req, res) {
     var routerUsername = req.params.username;
     var authenticatedUsername = req.user.username.toString();
     var authenticatedId = req.user._id.toString();
@@ -206,7 +87,7 @@ app.get('/users/:username', jsonParser, passport.authenticate('basic', {session:
 
 
 //should allow user to delete an account
-app.delete("/users/:username", jsonParser, passport.authenticate('basic', {session: false}), function(req, res) {
+app.delete("/users/:username", passport.authenticate('basic', {session: false}), function(req, res) {
     var routerUsername = req.params.username;
     var authenticatedUsername = req.user.username.toString();
 
@@ -222,7 +103,7 @@ app.delete("/users/:username", jsonParser, passport.authenticate('basic', {sessi
 });
 
 //allow a user to create a username
-app.post('/createuser', jsonParser, function(req, res) {
+app.post('/createuser', function(req, res) {
     if (!req.body) {
         return res.status(400).json({
             message: "No request body"
@@ -294,7 +175,7 @@ app.post('/createuser', jsonParser, function(req, res) {
     });
 });
 //Allows users to edit their password
-app.put("/users/:username", jsonParser, passport.authenticate('basic', {
+app.put("/users/:username", passport.authenticate('basic', {
         session: false
     }), function(req, res) {
         //req to varibles
