0f395261235d3dcad119f02ecdfc0590524979e9
KevL927 <leekevin@gmail.com>
2017-01-24 05:07:33 -0500

    Refactor actions and reducers into ES5

--- client/js/reducers/reducers.js
+++ client/js/reducers/reducers.js
@@ -1,21 +1,21 @@
-import actions from '../actions/actions';
+var actions = require('../actions/actions');
 
-const REGISTER_SUCCESS = actions.REGISTER_SUCCESS,
-      REGISTER_ERROR = actions.REGISTER_ERROR,
-      LOGIN_SUCCESSFUL = actions.LOGIN_SUCCESSFUL,
-      LOGIN_FAIL = actions.LOGIN_FAIL,
-      FETCH_STICKIES_SUCCESS = actions.FETCH_STICKIES_SUCCESS,
-      FETCH_STICKIES_ERROR = actions.FETCH_STICKIES_ERROR,
-      POST_STICKY_SUCCESS = actions.POST_STICKY_SUCCESS,
-      POST_STICKY_ERROR = actions.POST_STICKY_ERROR,
-      EDIT_STICKY_SUCCESS = actions.EDIT_STICKY_SUCCESS,
-      EDIT_STICKY_ERROR = actions.EDIT_STICKY_ERROR,
-      DELETE_STICKY_SUCCESS = actions.EDIT_STICKY_ERROR,
-      DELETE_STICKY_ERROR = actions.EDIT_STICKY_ERROR,
-      LOGOUT_USER_NOW = actions.LOGOUT_USER_NOW,
-      LOGOUT_USER_ERROR = actions.LOGOUT_USER_ERROR;
+var REGISTER_SUCCESS = actions.REGISTER_SUCCESS,
+    REGISTER_ERROR = actions.REGISTER_ERROR,
+    LOGIN_SUCCESSFUL = actions.LOGIN_SUCCESSFUL,
+    LOGIN_FAIL = actions.LOGIN_FAIL,
+    FETCH_STICKIES_SUCCESS = actions.FETCH_STICKIES_SUCCESS,
+    FETCH_STICKIES_ERROR = actions.FETCH_STICKIES_ERROR,
+    POST_STICKY_SUCCESS = actions.POST_STICKY_SUCCESS,
+    POST_STICKY_ERROR = actions.POST_STICKY_ERROR,
+    EDIT_STICKY_SUCCESS = actions.EDIT_STICKY_SUCCESS,
+    EDIT_STICKY_ERROR = actions.EDIT_STICKY_ERROR,
+    DELETE_STICKY_SUCCESS = actions.EDIT_STICKY_ERROR,
+    DELETE_STICKY_ERROR = actions.EDIT_STICKY_ERROR,
+    LOGOUT_USER_NOW = actions.LOGOUT_USER_NOW,
+    LOGOUT_USER_ERROR = actions.LOGOUT_USER_ERROR;
 
-const initialState = {
+var initialState = {
       stickies: [],
       loginError: null,
       fetchGetError: null,
@@ -29,105 +29,85 @@ const initialState = {
 };
 
 function stickyReducer(state, action) {
-    var newState;
-
     state = state || initialState;
 
     switch(action.type) {
-
         case REGISTER_ERROR:
-            newState = Object.assign({}, state, {
+            return Object.assign({}, state, {
                 registerError: true,
                 error: action.payload
             });
-            return newState;
 
         case LOGIN_SUCCESSFUL:
-            newState = Object.assign({}, state, {
+            return Object.assign({}, state, {
                 isAuthenticated: true,
                 hash: action.payloadHash,
                 currentUser: action.payloadUsername
             });
-            return newState;
 
         case LOGIN_FAIL:
-            newState = Object.assign({}, state, {
+            return Object.assign({}, state, {
                 isAuthenticated: false,
                 error: action.payload
             });
-            return newState;
 
         case LOGOUT_USER_NOW:
-            newState = Object.assign({}, state, {
-                    stickies: [],
-                    loginError: null,
-                    logoutError: null,
-                    fetchGetError: null,
-                    fetchPostError: null,
-                    isAuthenticated: false,
-                    registerError: null,
-                    hash: null,
-                    currentUser: null,
-                    previousUser: action.payload
+            return Object.assign({}, state, {
+                stickies: [],
+                loginError: null,
+                logoutError: null,
+                fetchGetError: null,
+                fetchPostError: null,
+                isAuthenticated: false,
+                registerError: null,
+                hash: null,
+                currentUser: null,
+                previousUser: action.payload
             });
-            return newState;
 
         case LOGOUT_USER_ERROR:
-            newState = Object.assign({}, state, {
+            return Object.assign({}, state, {
                     logoutError: null
             })
-            return newState;
 
         case FETCH_STICKIES_SUCCESS:
-            newState = Object.assign({}, state, {
+            return Object.assign({}, state, {
                 stickies: action.payload,
                 fetchGetError: null
             });
-            return newState;
 
         case FETCH_STICKIES_ERROR:
-            newState = Object.assign({}, state, {
+            return Object.assign({}, state, {
                 fetchGetError: action.payload
             });
-            return newState;
 
         case POST_STICKY_SUCCESS:
-            newState = Object.assign({}, state, {
+            return Object.assign({}, state, {
                 stickies: state.stickies.concat({
-                    stickyId: action.payloadStickyId,
-                    title: action.payloadTitle,
-                    content: action.payloadContent
+                stickyId: action.payloadStickyId,
+                title: action.payloadTitle,
+                content: action.payloadContent
                 })
-            })
-            return newState;
+            });
 
         case POST_STICKY_ERROR:
-            newState = Object.assign({}, state, {
+            return Object.assign({}, state, {
                 fetchPostError: action.payload
             });
-            return newState;
-
-        case EDIT_STICKY_SUCCESS:
-            return newState;
 
         case EDIT_STICKY_ERROR:
-            newState = Object.assign({}, state, {
+            return Object.assign({}, state, {
                 editPostError: action.payload
             });
-            return newState;
-
-        case DELETE_STICKY_SUCCESS:
-            return newState;
 
         case DELETE_STICKY_ERROR:
-            newState = Object.assign({}, state, {
+            return Object.assign({}, state, {
                 deletePostError: action.payload
             });
-            return newState;
 
         default:
             return state;
     }
 }
 
-export default stickyReducer;
+exports.stickyReducer = stickyReducer;
