2bdbbcafaf9e06637c4790801ed9b8f3b01eb2fe
Lavie Ruan <lavieruan@Lavies-MacBook.local>
2017-01-25 17:02:19 -0600

    fix open and close hamburger

--- client/js/components/Header.js
+++ client/js/components/Header.js
@@ -7,64 +7,89 @@ import MenuItem from 'material-ui/MenuItem';
 import IconButton from 'material-ui/IconButton';
 import Menu from 'material-ui/svg-icons/navigation/menu';
 
-module.exports = function(props) {
-    var styles ={
-        iconcolor: {
-            color:'white'
+module.exports = React.createClass({
+    getInitialState: function() {
+        return {
+            open: false
         }
-    };
-    if (!props.currentUser) {
-        return <span className="navBar">
-        <AppBar
-        showMenuIconButton={false} 
-        title="Bethinker">
-            <IconMenu
-                iconButtonElement={
-                    <IconButton
-                        id="icon-button"
-                    >
-                        <Menu/>
-                    </IconButton>
-                }
-                anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-                targetOrigin={{horizontal: 'right', vertical: 'top'}}
-            >   
-                <Link to="/login">
-                    <MenuItem primaryText="Login"/>
-                </Link>
-                <Link to="/register">
-                    <MenuItem primaryText="Register"/>
-                </Link>
-            </IconMenu>
-        </AppBar>
-        </span>
-    }
-    else {
-        return <span className="navBar">
-        <AppBar 
-        showMenuIconButton={false}
-        title="Bethinker"
-        >
-            <IconMenu
+    },
+    handleOpen: function() {
+        this.setState({open: true});
+    },
+
+    handleClose: function() {
+        this.setState({open: false});
+    },
+    render: function () {
+        if (!this.props.currentUser) {
+            return <span className="navBar">
+            <AppBar
+            showMenuIconButton={false} 
+            title="Bethinker">
+                <IconMenu
                     iconButtonElement={
-                        <IconButton  
-                            style={styles.iconcolor}
+                        <IconButton
+                            id="icon-button"
                         >
                             <Menu/>
                         </IconButton>
                     }
                     anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-                    targetOrigin={{horizontal: 'right', vertical: 'top'}}
+                    targetOrigin={{horizontal: 'right', vertical: 'bottom'}}
+                    open={this.state.open}
+                    onTouchTap={this.handleOpen}
                 >   
-                <Link to="/">
-                    <MenuItem primaryText="Home"/>
-                </Link>
-                <Link to="/logout">
-                    <MenuItem primaryText="Logout"/>
-                </Link>
+                    <Link to="/login">
+                        <MenuItem 
+                            primaryText="Login"
+                            onTouchTap={this.handleClose}
+                        />
+                    </Link>
+                    <Link to="/register">
+                        <MenuItem 
+                            primaryText="Register"
+                            onTouchTap={this.handleClose}
+                        />
+                    </Link>
+                </IconMenu>
+            </AppBar>
+            </span>
+        }
+        else {
+            return <span className="navBar">
+            <AppBar 
+            showMenuIconButton={false}
+            title="Bethinker"
+            >
+                <IconMenu
+                        iconButtonElement={
+                            <IconButton  
+                                id="icon-button"
+                            >
+                                <Menu/>
+                            </IconButton>
+                        }
+                        anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+                        targetOrigin={{horizontal: 'right', vertical: 'bottom'}}
+                        open={this.state.open}
+                        onTouchTap={this.handleOpen}
+                    >   
+                    <Link to="/">
+                        <MenuItem 
+                            primaryText="Home"
+                            onTouchTap={this.handleClose}
+                        />
+                    </Link>
+                    <Link to="/logout">
+                        <MenuItem 
+                            primaryText="Logout"
+                            onTouchTap={this.handleClose}
+                        />
+                    </Link>
 
-            </IconMenu>
-        </AppBar>
-        </span>
+                </IconMenu>
+            </AppBar>
+            </span>
+        }
     }
-}
+});
