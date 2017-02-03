0579f9df8c0dec4895673bd8932de3b0bd9cb799
KL012 <kevl927@gmail.com>
2017-02-03 00:46:27 +0000

    Remove explanatory comments and remove unnecessary spaces

--- server/endpoints/stickyRouter.js
+++ server/endpoints/stickyRouter.js
@@ -7,7 +7,6 @@ var stickyRouter = express.Router();
 
 
 stickyRouter.get('/users/:username/stickies', passport.authenticate('basic', {session: false}), function(req, res) {
-
     var routerUsername = req.params.username;
     var authenticatedUsername = req.user.username.toString();
     var authenticatedId = req.user._id.toString();
@@ -26,34 +25,30 @@ stickyRouter.get('/users/:username/stickies', passport.authenticate('basic', {se
     });
 });
 
-//should allow authorized users to delete their sticky
 stickyRouter.delete("/users/:username/stickies/:stickyId", passport.authenticate('basic', {session: false}), function(req, res) {
-    //putting all requests in variables
-            var username = req.params.username;
-            var stickyId = req.params.stickyId;
-            var authenticatedUsername = req.user.username.toString();
+    var username = req.params.username;
+    var stickyId = req.params.stickyId;
+    var authenticatedUsername = req.user.username.toString();
 
-    //finds to allow user to delete sticky by stick:id
     Sticky.findByIdAndRemove({_id: stickyId}, function(err, sticky) {
-            if(err) {
-                return res.sendStatus(500);
-            }
-            if(username !== authenticatedUsername) {
-                return res.status(401).json({message: "Unauthorized"});
-            }
+        if(err) {
+            return res.sendStatus(500);
+        }
+        if(username !== authenticatedUsername) {
+            return res.status(401).json({message: "Unauthorized"});
+        }
         return res.status(200).json({});
-
     });
 });
 
 //Allows users to create the title for their sticky notes
 stickyRouter.post('/users/:username/stickies', passport.authenticate('basic', {session: false}), function(req, res) {
-            var routeUsername = req.params.username;
-            var stickyId = req.params.stickyId;
-            var authenticatedUsername = req.user.username.toString();
-            var authenticatedId = req.user._id.toString();
-            var title = req.body.title;
-            var content = req.body.content;
+    var routeUsername = req.params.username;
+    var stickyId = req.params.stickyId;
+    var authenticatedUsername = req.user.username.toString();
+    var authenticatedId = req.user._id.toString();
+    var title = req.body.title;
+    var content = req.body.content;
 
     Sticky.create({title: title, content: content, _user: authenticatedId}, function(err, sticky) {
         if(err) {
@@ -68,50 +63,45 @@ stickyRouter.post('/users/:username/stickies', passport.authenticate('basic', {s
 
 });
 
-//Allows user to edit a sticky note
 stickyRouter.put("/users/:username/stickies/:stickyId", passport.authenticate('basic', {session: false}), function(req, res) {
-    //putting all requests in variables
-            var routerUsername = req.params.username;
-            var stickyId = req.params.stickyId;
-            var title = req.body.title || "";
-            var content = req.body.content;
-            var authenticatedUsername = req.user.username.toString();
-            var authenticatedId = req.user._id.toString();
+    var routerUsername = req.params.username;
+    var stickyId = req.params.stickyId;
+    var title = req.body.title || "";
+    var content = req.body.content;
+    var authenticatedUsername = req.user.username.toString();
+    var authenticatedId = req.user._id.toString();
 
-    //finds stick-id and then allow user to edit sticky
     Sticky.findOne({_user: authenticatedId, _id: stickyId}, function(err, sticky) {
+        if(routerUsername !== authenticatedUsername) {
+            return res.status(401).json({message: "Unauthorized"});
+        }
 
-            if(routerUsername !== authenticatedUsername) {
-
-                return res.status(401).json({message: "Unauthorized"});
-            }
-
-            //check to see if title or content was changed
-            if (title && content){
-                Sticky.findOneAndUpdate(
-                    {_user: authenticatedId, _id: stickyId},
-                    {content: content, title: title},
-                    function(err, sticky) {
-                        if(err) {
-                            return res.sendStatus(500);
-                        }
-                        return res.status(200).json({message: "Succesfully saved"});
-                        }
-                );
-            }
-            //if title is an empty string, !title will return true, content will get updated
-            if (content && !title){
-                Sticky.findOneAndUpdate(
-                    {_user: authenticatedId, _id: stickyId},
-                    {title: sticky.title, content: content},
-                    function(err, sticky) {
-                        if(err) {
-                            return res.sendStatus(500);
-                        }
+        //check to see if title or content was changed
+        if (title && content){
+            Sticky.findOneAndUpdate(
+                {_user: authenticatedId, _id: stickyId},
+                {content: content, title: title},
+                function(err, sticky) {
+                    if(err) {
+                        return res.sendStatus(500);
+                    }
+                    return res.status(200).json({message: "Succesfully saved"});
+                    }
+            );
+        }
+        //if title is an empty string, !title will return true, content will get updated
+        if (content && !title){
+            Sticky.findOneAndUpdate(
+                {_user: authenticatedId, _id: stickyId},
+                {title: sticky.title, content: content},
+                function(err, sticky) {
+                    if(err) {
+                        return res.sendStatus(500);
+                    }
                     return res.status(201).json({message: "Succesfully saved"});
-                });
-            }
-
+                }
+            );
+        }
     });
 });
 
