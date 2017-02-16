306985bc8bb953886269f27f17bd867df921fad6
Lavie Ruan <lavieruan@Lavies-MacBook.local>
2017-02-15 23:30:35 -0600

    Add parenthesis to return

--- client/js/components/Header.js
+++ client/js/components/Header.js
@@ -27,44 +27,46 @@ module.exports = React.createClass({
     },
     render: function () {
         if (!this.props.currentUser) {
-            return <span className="navBar">
-                        <AppBar
-                        showMenuIconButton={false} 
-                        title={
-                            <Link 
-                              to="/"
-                              id="header-link"
-                            >
-                              <span id="bethinker-heading">Bethinker</span>
-                            </Link>
-                        }
+            return (
+                <span className="navBar">
+                    <AppBar
+                    showMenuIconButton={false} 
+                    title={
+                        <Link 
+                          to="/"
+                          id="header-link"
                         >
-                            <IconMenu
-                                iconButtonElement={
-                                    <IconButton
-                                        id="icon-button"
-                                        onClick={this.handleOpenCloseToggle}
-                                    >
-                                        <Menu 
-                                            onBlur={this.handleClose}
-                                        />
-                                    </IconButton>
-                                }
-                                anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-                                targetOrigin={{horizontal: 'right', vertical: 'bottom'}}
-                                open={this.state.open}
-                                onClick={this.handleOpenCloseToggle}
-                                onItemTouchTap={this.handleOpenCloseToggle}
-                            >   
-                                <Link to="/login">
-                                    <MenuItem primaryText="Login"/>
-                                </Link>
-                                <Link to="/register">
-                                    <MenuItem primaryText="Register"/>
-                                </Link>
-                            </IconMenu>
-                        </AppBar>
-                    </span>;
+                          <span id="bethinker-heading">Bethinker</span>
+                        </Link>
+                    }
+                    >
+                        <IconMenu
+                            iconButtonElement={
+                                <IconButton
+                                    id="icon-button"
+                                    onClick={this.handleOpenCloseToggle}
+                                >
+                                    <Menu 
+                                        onBlur={this.handleClose}
+                                    />
+                                </IconButton>
+                            }
+                            anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+                            targetOrigin={{horizontal: 'right', vertical: 'bottom'}}
+                            open={this.state.open}
+                            onClick={this.handleOpenCloseToggle}
+                            onItemTouchTap={this.handleOpenCloseToggle}
+                        >   
+                            <Link to="/login">
+                                <MenuItem primaryText="Login"/>
+                            </Link>
+                            <Link to="/register">
+                                <MenuItem primaryText="Register"/>
+                            </Link>
+                        </IconMenu>
+                    </AppBar>
+                </span>
+            );
         }
         else {
             return <span className="navBar">
